277988bcb6c772e5c8061f6cf937ad7f81b9c2ad
vaniog <vaniog@yandex-team.ru>
2025-04-10 00:22:25 +0300

    lint fix

--- server/pkg/usecase/state/middlewares.go
+++ server/pkg/usecase/state/middlewares.go
@@ -3,7 +3,7 @@ package state
 type MiddlewareFunc[SIn, SOut any, EIn, EOut any] func(
 	c *Context[SIn], event EIn, next HandlerFunc[SOut, EOut]) error
 
-func WithMiddleware[SIn, SOut any, EIn, EOut any](
+func WithMiddleware[SIn, SOut, EIn, EOut any](
 	middleware MiddlewareFunc[SIn, SOut, EIn, EOut],
 	next HandlerFunc[SOut, EOut],
 ) HandlerFunc[SIn, EIn] {
