8701153005f2e5083c80b9a16088e9e1159c2904
Mike de Geofroy <mikedegeofroy@gmail.com>
2025-05-31 11:57:06 +0300

    Expo Go -> Development Build

--- client/App.tsx
+++ client/App.tsx
@@ -1,37 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import MapView, { UrlTile } from 'react-native-maps';
-
-const MAPBOX_TOKEN =
-  'pk.eyJ1IjoibWlrZWRlZ2VvZnJveSIsImEiOiJja3ZiOGQwc3I0N29uMnVxd2xlbGVyZGQzIn0.11XK5mqIzfLBTfNTYOGDgw';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView
-        provider='google'
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 59.943801,
-          longitude: 30.335833,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
-        mapType='none'
-        showsCompass={false}
-        showsBuildings={false}
-        showsIndoors={false}
-        showsIndoorLevelPicker={false}
-        onPanDrag={() => {}}
-      >
-        <UrlTile
-          urlTemplate={`https://api.mapbox.com/styles/v1/mikedegeofroy/cma57ielt004801s3gkciegzd/tiles/512/{z}/{x}/{y}?access_token=${MAPBOX_TOKEN}&format=webp`}
-          maximumZ={22}
-          flipY={false}
-          tileSize={256}
-        />
-      </MapView>
-
       <StatusBar style='auto' />
     </View>
   );
